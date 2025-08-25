adf6713a841adf4bce29f73e90c6784f3434e559
SiHyun Park <kaeny1008@gmail.com>
2025-08-25 17:33:39 +0900

    2차 수정

--- GisanParkGolf/Pages/Manager/GameResult.cshtml.cs
+++ GisanParkGolf/Pages/Manager/GameResult.cshtml.cs
@@ -1,118 +1,62 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GiSanParkGolf.Pages.Manager
 {
     public class GameResultModel : PageModel
     {
-        [BindProperty] public string GameCode { get; set; }
-        [BindProperty] public string CourseName { get; set; }
-        [BindProperty] public int HoleNumber { get; set; }
-
-        // 검색 및 페이징
-        [BindProperty(SupportsGet = true)] public string SearchParticipant { get; set; }
-        [BindProperty(SupportsGet = true)] public string SearchTeam { get; set; }
-        [BindProperty(SupportsGet = true)] public int PageIndex { get; set; } = 1;
-        public int PageSize { get; set; } = 10;
-        public int TotalCount { get; set; }
-        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
-
-        public List<SelectListItem> GameOptions { get; set; } = new();
-        public List<SelectListItem> CourseOptions { get; set; } = new();
-        public List<SelectListItem> HoleOptions { get; set; } = new();
-
-        public List<HoleResultRow> HoleResults { get; set; } = new();
-        public List<HoleResultRow> PagedHoleResults { get; set; } = new();
-
-        public List<CourseScoreCard> ScoreCardCourses { get; set; } = new();
+        public List<TeamScoreCourse> TeamScoreCourses { get; set; } = new();
 
         public void OnGet()
         {
-            // 예시 데이터: 실제로는 DB에서 대회/코스/홀/참가자 정보 불러옴
-            ScoreCardCourses = new List<CourseScoreCard>
+            // 예시 데이터: 코스별, 팀단위로 생성
+            TeamScoreCourses = new List<TeamScoreCourse>
             {
-                new CourseScoreCard
+                new TeamScoreCourse
                 {
                     CourseName = "A 코스",
                     HoleNumbers = Enumerable.Range(1,9).ToList(),
-                    Participants = new List<ScoreCardParticipant>
+                    TeamRows = new List<TeamScoreRow>
                     {
-                        new ScoreCardParticipant { CourseOrder=1, ParticipantName="회원2", ParticipantId="A122" },
-                        new ScoreCardParticipant { CourseOrder=2, ParticipantName="회원4", ParticipantId="A125" },
-                        new ScoreCardParticipant { CourseOrder=3, ParticipantName="회원3", ParticipantId="A123" },
-                        new ScoreCardParticipant { CourseOrder=4, ParticipantName="회원44", ParticipantId="A124" },
-                        new ScoreCardParticipant { CourseOrder=5, ParticipantName="관리자", ParticipantId="1" }
+                        new TeamScoreRow { CourseOrder=1, TeamNumber="T1", ParticipantName="회원2", ParticipantId="A122" },
+                        new TeamScoreRow { CourseOrder=2, TeamNumber="T1", ParticipantName="회원4", ParticipantId="A125" },
+                        new TeamScoreRow { CourseOrder=3, TeamNumber="T1", ParticipantName="회원3", ParticipantId="A123" },
+                        new TeamScoreRow { CourseOrder=4, TeamNumber="T1", ParticipantName="회원44", ParticipantId="A124" },
+                        new TeamScoreRow { CourseOrder=5, TeamNumber="T1", ParticipantName="관리자", ParticipantId="1" }
                     }
                 },
-                new CourseScoreCard
+                new TeamScoreCourse
                 {
                     CourseName = "B 코스",
                     HoleNumbers = Enumerable.Range(1,9).ToList(),
-                    Participants = new List<ScoreCardParticipant>
+                    TeamRows = new List<TeamScoreRow>
                     {
-                        new ScoreCardParticipant { CourseOrder=1, ParticipantName="회원2", ParticipantId="A122" },
-                        new ScoreCardParticipant { CourseOrder=2, ParticipantName="회원4", ParticipantId="A125" },
-                        new ScoreCardParticipant { CourseOrder=3, ParticipantName="회원3", ParticipantId="A123" },
-                        new ScoreCardParticipant { CourseOrder=4, ParticipantName="회원44", ParticipantId="A124" },
-                        new ScoreCardParticipant { CourseOrder=5, ParticipantName="관리자", ParticipantId="1" }
+                        new TeamScoreRow { CourseOrder=1, TeamNumber="T2", ParticipantName="회원2", ParticipantId="A122" },
+                        new TeamScoreRow { CourseOrder=2, TeamNumber="T2", ParticipantName="회원4", ParticipantId="A125" },
+                        new TeamScoreRow { CourseOrder=3, TeamNumber="T2", ParticipantName="회원3", ParticipantId="A123" },
+                        new TeamScoreRow { CourseOrder=4, TeamNumber="T2", ParticipantName="회원44", ParticipantId="A124" },
+                        new TeamScoreRow { CourseOrder=5, TeamNumber="T2", ParticipantName="관리자", ParticipantId="1" }
                     }
                 }
             };
+            // 점수는 처음엔 빈 값. 필요하면 TeamRows[i].Scores[h] = 점수; 형태로 채움
         }
 
-        public class CourseScoreCard
+        public class TeamScoreCourse
         {
             public string CourseName { get; set; }
             public List<int> HoleNumbers { get; set; } = new();
-            public List<ScoreCardParticipant> Participants { get; set; } = new();
+            public List<TeamScoreRow> TeamRows { get; set; } = new();
         }
 
-        public class ScoreCardParticipant
+        public class TeamScoreRow
         {
             public int CourseOrder { get; set; }
+            public string TeamNumber { get; set; }
             public string ParticipantName { get; set; }
             public string ParticipantId { get; set; }
             public Dictionary<int, int> Scores { get; set; } = new(); // key: 홀번호, value: 점수
-        }
-
-        public IActionResult OnPost()
-        {
-            // 저장 로직
-            TempData["SuccessMessage"] = "결과가 저장되었습니다.";
-            return Page();
-        }
-
-        public JsonResult OnGetCourses(string gameCode)
-        {
-            // 실제 DB에서 해당 대회 코스 목록 조회
-            var list = new List<SelectListItem> {
-        new("A코스", "A"),
-        new("B코스", "B")
-    };
-            return new JsonResult(list.Select(x => new { value = x.Value, text = x.Text }));
-        }
-
-        public JsonResult OnGetHoles(string gameCode, string courseName)
-        {
-            // 실제 DB에서 해당 코스의 홀 목록 조회
-            var list = new List<SelectListItem> {
-        new("1번홀", "1"),
-        new("2번홀", "2")
-    };
-            return new JsonResult(list.Select(x => new { value = x.Value, text = x.Text }));
-        }
-
-        public class HoleResultRow
-        {
-            public string CourseName { get; set; }
-            public int HoleNumber { get; set; }
-            public string TeamNumber { get; set; }
-            public int CourseOrder { get; set; }
-            public string ParticipantName { get; set; }
-            public string ParticipantId { get; set; }
-            public int Score { get; set; }
-            public string Note { get; set; }
         }
     }
 }
